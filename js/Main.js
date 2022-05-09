0807a56bbb723341ef1d0b5ce6e682a6adb10c54
InDaPlaZ <benjaminspider@hotmail.fr>
2022-05-09 13:58:25 +0200

    recup player

---
+++ js/Main.js
@@ -0,0 +1,11 @@
+
+//initialize
+
+//make new instance of the Game object
+//Object player 1
+let playerOne = new Player("playerOne", Soigneur(20, 3));
+//disableFight(playerOne);
+//Object player 2
+let playerTwo = new Player("playerTwo", Mage(20, 3));
+
+console.log(playerOne, playerTwo)
--- js/Player.js
+++ js/Player.js
@@ -1,8 +1,64 @@
-class Player{
+class Player {
     name;
-    personnage;
-    constructor(playerName){  
+    personnage = new Player();
+    constructor(playerName) {
         this.name = playerName;
-        
+        this.personnage = this.personnage
     }
+
+
 }
+class Mage extends Player {
+    pv;
+    attack;
+    constructor(pv, attack) {
+        this.pv = pv;
+        this.attack = attack
+    }
+
+    getPv() {
+        return this.pv;
+    }
+
+    getAttack(){
+        return this.attack
+    }
+
+}
+
+class Guerrier extends Player {
+    pv;
+    attack;
+    constructor(pv, attack) {
+        this.pv = pv;
+        this.attack = attack
+    }
+
+    getPv() {
+        return this.pv;
+    }
+
+    getAttack(){
+        return this.attack
+    }
+
+}
+
+class Soigneur extends Player {
+    pv;
+    attack;
+    constructor(pv, attack) {
+        this.pv = pv;
+        this.attack = attack
+    }
+
+    getPv() {
+        return this.pv;
+    }
+
+    getAttack(){
+        return this.attack
+    }
+
+}
+
